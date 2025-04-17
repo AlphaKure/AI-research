1b07adf9ac83d26e399abbaea2ff329681c8b086
alpha <kurealphaoxo@gmail.com>
2025-04-17 22:28:23 +0800

    Remove WSL Detect

--- src/utils/system.py
+++ src/utils/system.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import platform
-from functools import lru_cache
 from typing import ClassVar,Literal
 
 class System:
@@ -8,17 +7,7 @@ class System:
     root : ClassVar[Path] = Path(__file__).resolve().parent.parent # point to src
 
     operation_system: ClassVar[Literal["Windows","Linux"]] = platform.system()
-    
-    @classmethod
-    @lru_cache(maxsize=1) # 只運算一次
-    def is_wsl(cls) -> bool:
-        if cls.operation_system == "Windows":
-            return False
-        else:
-            if "microsoft" or "wsl" in platform.uname().release.lower():
-                return True
-            return False
 
     @classmethod
     def get_os_info(cls):
-        return {"operation_system":cls.operation_system,"is_wsl":cls.is_wsl()}
+        return {"operation_system":cls.operation_system}
